3d38f8a4d60a501033984ed960c062f250fe4ca4
Kristijan Jovev <Kristijan Jovev>
2017-04-28 18:55:00 +0200

    Change to Examples page

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -3,7 +3,7 @@ var React = require('react');
 const Examples = (props) => {
   return (
     <div>
-      <h4>Examples component</h4>
+      <h4>Examples</h4>
       <p className="welcome-text">Welcome to examples page!</p>
     </div>
   );

f4fd8a04837eeeb672b72c5f85e98d844aa0b00a
Kristijan Jovev <Kristijan Jovev>
2017-05-05 22:50:01 +0200

    Add styles to about page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -11,8 +11,17 @@ var React = require('react');
 const About = (props) => {
   return (
     <div>
-      <h4>About component</h4>
-      <p className="welcome-text">Welcome to the About page!</p>
+      <h2 className="text-center">About </h2>
+      <p className="welcome-text text-center">
+        Welcome to the About page for the React Weather!
+        <br/><br/>
+        The React Weather app uses an API that fetches weather data
+        and displays the temperature on the screen for your hometown.
+        <br/><br/>
+        React Weather uses the API from &nbsp;
+        <a href="http://openweathermap.org/" target="blank">Open Weather Map</a>&nbsp;
+        and returns the result to you!
+      </p>
     </div>
   );
 };
